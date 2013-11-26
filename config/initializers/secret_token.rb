d005b3fab7ed7fa4d1ec89b2387fb9ad851a2e7f
anstak <anstak@gmail.com>
2013-11-26 12:08:22 +0300

    Init pays

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pays::Application.config.secret_key_base = '1ef2f8e92b4e562c6d3b23d5fd1bc07857dcbd262f9e9c65bab2d5264b318ee415979e09fa66e059882969fae664c887257b8e5e57209ccfa111ddbfa066186c'
